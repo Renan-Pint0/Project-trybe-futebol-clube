def7ce241ce4fa5bedcfc28f1df7f35704479b7d
Renan Pinto <renanrnpinto@gmail.com>
2023-01-25 20:34:32 -0300

    Adicionando migrations e models

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,63 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: number;
+}
+
+Matches.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    field: 'home_team_id',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'home_team_goals',
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    field: 'away_team_id',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: BOOLEAN,
+    field: 'in_progress',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Matches;
---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
